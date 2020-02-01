cf452c4a3ea495f065df05521fd263ed9bf3e6b6
lkmburu <shamukenya@gmail.com>
2020-02-01 11:15:29 +0300

    POS PRAC

---
+++ BSE_APP_v3/Obj/Resources.d
@@ -0,0 +1,9 @@
+C:\Users\smburu\Desktop\BSE_APP_all\BSE_APP_v3\GOAL\Resources\APCLESSGuiPresent.TBR;1523427602000
+C:\Users\smburu\Desktop\BSE_APP_all\BSE_APP_v3\GOAL\Resources\APCLESSGuiPresent.gml;1522225808000
+C:\Users\smburu\Desktop\BSE_APP_all\BSE_APP_v3\GOAL\Resources\APCLESSGuiText.TBR;1522157734000
+C:\Users\smburu\Desktop\BSE_APP_all\BSE_APP_v3\GOAL\Resources\APCLESSGuiText.gml;1522225808000
+C:\Users\smburu\Desktop\BSE_APP_all\BSE_APP_v3\GOAL\Resources\COL_NMB.JPG;1520411952000
+C:\Users\smburu\Desktop\BSE_APP_all\BSE_APP_v3\GOAL\Resources\Icons\CLess.WGU;1500990738000
+C:\Users\smburu\Desktop\BSE_APP_all\BSE_APP_v3\GOAL\Resources\Icons\COL_NMB.JPG;1520411952000
+C:\Users\smburu\Desktop\BSE_APP_all\BSE_APP_v3\GOAL\Resources\LOGO.png;1500990738000
+gmlToTbrConverter
